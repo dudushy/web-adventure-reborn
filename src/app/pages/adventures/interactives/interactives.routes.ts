60ee60ecf082af2f496b614348690bf9244f341c
dudushy <eduardotalarico1337@gmail.com>
2026-05-21 22:17:23 -0300

    Format

--- src/app/pages/adventures/interactives/interactives.routes.ts
+++ src/app/pages/adventures/interactives/interactives.routes.ts
@@ -2,6 +2,5 @@ import { Routes } from '@angular/router';
 
 export default [
   // { path: '', redirectTo: 'temp', pathMatch: 'full' },
-
   // { path: 'temp', loadComponent: () => import('./temp/temp').then((m) => m.Temp) },
 ] satisfies Routes;
