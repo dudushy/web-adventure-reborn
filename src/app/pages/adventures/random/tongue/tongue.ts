b4540b7aae46c9150c2cc6116da22dacd74b4dca
dudushy <eduardotalarico1337@gmail.com>
2026-05-23 23:38:28 -0300

    Creating tongue (wip)

--- src/app/pages/adventures/random/tongue/tongue.ts
+++ src/app/pages/adventures/random/tongue/tongue.ts
@@ -1,13 +1,16 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { DebugService } from '@shyland-dev/utils';
 
 @Component({
   selector: 'app-adventure-random-tongue',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './tongue.html',
   styleUrl: './tongue.scss',
 })
 export class Tongue implements OnInit, OnDestroy {
+  isOpen: boolean | null = null;
+
   constructor(private debugService: DebugService) {
     this.debugService.log(this);
   }
@@ -18,5 +21,13 @@ export class Tongue implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.debugService.log(this);
+  }
+
+  toggleMouth(): void {
+    this.debugService.log(this, '(BEFORE) this.isOpen', this.isOpen);
+
+    this.isOpen = this.isOpen === null ? true : !this.isOpen;
+
+    this.debugService.log(this, '(AFTER) this.isOpen', this.isOpen);
   }
 }
