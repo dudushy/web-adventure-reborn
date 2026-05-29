8f1f4b9ae1c4dcfcd07677a472e1152b9556506c
dudushy <eduardotalarico1337@gmail.com>
2026-05-29 20:00:09 -0300

    Refactor app configuration and component by removing unused imports and simplifying router setup

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,5 @@
-import { DOCUMENT } from '@angular/common';
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
-import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { DebugService } from '@shyland-dev/utils';
 import { routes } from './app.routes';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
@@ -15,14 +14,11 @@ import { IconComponent } from '@shyland-dev/ui';
 })
 export class App implements OnInit, OnDestroy {
   readonly navRoutes = routes.filter((r) => r.path && r.path !== '**');
-  private readonly document = inject(DOCUMENT);
-  private readonly teardownListeners: Array<() => void> = [];
 
   isOpenSideMenu = false;
 
   constructor(
     private debugService: DebugService,
-    private router: Router,
     private storageService: StorageService,
     private translateService: TranslateService,
     private themeService: ThemeService,
@@ -32,8 +28,6 @@ export class App implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.debugService.log(this);
-    this.ensureHomeForDefaultEntry('init');
-    this.registerResumeFallback();
 
     const language = this.storageService.get('language') as string;
     this.debugService.log(this, 'language', language);
@@ -46,74 +40,12 @@ export class App implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    this.teardownListeners.forEach((teardown) => teardown());
-    this.teardownListeners.length = 0;
+    this.debugService.log(this);
   }
 
   setSideMenu(open: boolean): void {
     this.debugService.log(this, 'open', open);
 
     this.isOpenSideMenu = open;
-  }
-
-  private registerResumeFallback(): void {
-    const win = this.document.defaultView;
-    if (!win) {
-      return;
-    }
-
-    const onPageShow = () => this.ensureHomeForDefaultEntry('pageshow');
-    const onVisibilityChange = () => {
-      if (this.document.visibilityState === 'visible') {
-        this.ensureHomeForDefaultEntry('visibilitychange');
-      }
-    };
-
-    win.addEventListener('pageshow', onPageShow);
-    this.document.addEventListener('visibilitychange', onVisibilityChange);
-
-    this.teardownListeners.push(
-      () => win.removeEventListener('pageshow', onPageShow),
-      () => this.document.removeEventListener('visibilitychange', onVisibilityChange),
-    );
-  }
-
-  private ensureHomeForDefaultEntry(trigger: string): void {
-    const pathname = this.document.defaultView?.location.pathname;
-    if (!pathname || !this.isDefaultEntryPath(pathname)) {
-      return;
-    }
-
-    this.debugService.log(this, 'redirect_default_entry', { trigger, pathname });
-    void this.router.navigateByUrl('/home', { replaceUrl: true });
-  }
-
-  private isDefaultEntryPath(pathname: string): boolean {
-    const normalizedPathname = this.normalizePath(pathname);
-    const basePath = this.getNormalizedBasePath();
-
-    if (normalizedPathname === '/' || normalizedPathname === '/index.html') {
-      return true;
-    }
-
-    if (basePath === '/') {
-      return false;
-    }
-
-    return normalizedPathname === basePath || normalizedPathname === `${basePath}/index.html`;
-  }
-
-  private getNormalizedBasePath(): string {
-    const baseHref = this.document.querySelector('base')?.getAttribute('href') ?? '/';
-    return this.normalizePath(baseHref);
-  }
-
-  private normalizePath(path: string): string {
-    const withoutOrigin = path.replace(/^https?:\/\/[^/]+/i, '');
-    const withoutQueryHash = withoutOrigin.split(/[?#]/, 1)[0] ?? '/';
-    const withLeadingSlash = withoutQueryHash.startsWith('/') ? withoutQueryHash : `/${withoutQueryHash}`;
-    const trimmedTrailingSlashes = withLeadingSlash.replace(/\/+$/, '');
-
-    return trimmedTrailingSlashes === '' ? '/' : trimmedTrailingSlashes;
   }
 }
