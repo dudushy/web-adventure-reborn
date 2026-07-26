434114c7fc30687ae464a26640f1ee51c6a9a818
dudushy <eduardotalarico1337@gmail.com>
2026-07-26 19:40:24 -0300

    Format and lint

--- src/app/pages/adventures/games/games.routes.ts
+++ src/app/pages/adventures/games/games.routes.ts
@@ -4,7 +4,10 @@ export default [
   { path: '', redirectTo: 'switch-frogs', pathMatch: 'full' },
 
   { path: 'switch-frogs', loadComponent: () => import('./switch-frogs/switch-frogs').then((m) => m.SwitchFrogs) },
-  { path: 'ultimate-tic-tac-toe', loadComponent: () => import('./ultimate-tic-tac-toe/ultimate-tic-tac-toe').then((m) => m.UltimateTicTacToe) },
+  {
+    path: 'ultimate-tic-tac-toe',
+    loadComponent: () => import('./ultimate-tic-tac-toe/ultimate-tic-tac-toe').then((m) => m.UltimateTicTacToe),
+  },
 
   { path: '**', redirectTo: '/adventures' },
 ] satisfies Routes;
